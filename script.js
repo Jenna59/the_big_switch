631f11f1671a26e5b68ccce70b8cf14bd8dcfcfb
Jennifer Cox <jennifer.cox@georgiancollege.ca>
2019-08-27 11:57:30 -0400

    Image compression, additional styling to button, changed paragraph font for better readability.

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ toggleBtn.addEventListener('click', function() {
     card.classList.toggle('flip');
     this.value = 'night';
     this.textContent = 'Night';
+    this.style.backgroundColor = '#23297a';
+    this.style.color = '#f2f3f4';
   } else if (this.value === 'night') {
     card.classList.toggle('flip');
     this.value = 'day';
     this.textContent = 'Day';
+    this.style.color = '#100c08';
+    this.style.backgroundColor = '#fefe22';
   }
 });
